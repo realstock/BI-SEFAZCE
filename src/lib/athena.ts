187b39d26b6d88c9a6837dafc9f96058d5f42a94
LEONARDO BATISTI DE FARIA <49774311@sefaz-ce.gov.br>
2026-04-20 11:09:47 -0300

    Fix Vercel reserved environment variables conflict

--- src/lib/athena.ts
+++ src/lib/athena.ts
@@ -7,7 +7,7 @@ export const athenaClient = new AthenaClient({
   region,
   endpoint: endpoint ? `https://${endpoint}` : undefined,
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID || "",
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || "",
+    accessKeyId: process.env.SEFAZ_AWS_ACCESS_KEY_ID || process.env.AWS_ACCESS_KEY_ID || "",
+    secretAccessKey: process.env.SEFAZ_AWS_SECRET_ACCESS_KEY || process.env.AWS_SECRET_ACCESS_KEY || "",
   },
 });
